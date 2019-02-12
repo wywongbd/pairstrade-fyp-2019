9cc2feb41ede986afe32f0036330d1498aac76de
wywongbd <wywongbd@github.com>
2019-02-11 22:37:39 -0800

    edited coint kalman strat

--- jupyter-py/ptstrategy_cointegration_kalman.py
+++ jupyter-py/ptstrategy_cointegration_kalman.py
@@ -98,11 +98,8 @@ class CointKalmanStrategy(PTStrategy):
             if self.consider_borrow_cost: 
                 PTStrategy.incur_borrow_cost(self.initial_price_data0, self.qty0)
             
-            if spread < self.lower_limit and self.alpha > 0:
+            if spread < self.lower_limit:
                 self.long_spread()
-                
-            elif spread < self.lower_limit and self.alpha <= 0:
-                self.exit_spread()
                 
             elif spread < self.up_medium:
                 self.exit_spread()
@@ -125,11 +122,8 @@ class CointKalmanStrategy(PTStrategy):
             if self.consider_borrow_cost: 
                 self.incur_borrow_cost(self.initial_price_data1, self.qty1)
             
-            if spread > self.upper_limit and self.alpha > 0:
+            if spread > self.upper_limit:
                 self.short_spread()
-            
-            elif spread > self.upper_limit and self.alpha <= 0:
-                self.exit_spread()
                 
             elif spread > self.low_medium:
                 self.exit_spread()
